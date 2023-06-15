d6a0a61bc79fd92f637bafd462eae50f78c5bf67
himali <himali@DESKTOP-D46JD1G>
2023-06-15 17:04:48 +0530

    helped successfully

--- help/src/help/helpdetails.java
+++ help/src/help/helpdetails.java
@@ -5,5 +5,6 @@ public class helpdetails {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("help details");
+System.out.println("help sended successfully");
 	}
 }
